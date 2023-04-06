9dbc22627e594397710c8a9a359690da3e246a34
tejHackerDev <tejasimha222@gmail.com>
2023-04-06 22:01:05 +0530

    feat: Add n function to connect to SurrealDB
    Expose connection to surrealDB, so that it can be used by  builder package, while generating part files
    
    Signed-off-by: tejHackerDev <tejasimha222@gmail.com>

--- lib/surrealdb_dart_orm_annotations.dart
+++ lib/surrealdb_dart_orm_annotations.dart
@@ -1,8 +1,28 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
-library surrealdb_dart_orm_annotations;
+library surrealdb_dart_annotations;
+
+import 'package:surrealdb_dart/surrealdb_dart.dart';
 
 export 'src/surrealdb_dart_orm_annotations_base.dart';
 
-// TODO: Export any libraries intended for clients of this package.
+/// An instance of SurrealDB connection, which shouldn't be used
+/// or modified by anyone other than the part files generated
+/// by `surrealdb_dart_orm` builder package.
+///
+/// This variable will be set only after calling [connect] function.
+/// So without invoking [connect] accessing this variable via any
+/// part file will throw runtimeException.
+late final Surreal surrealDB;
+
+/// Connects to SurrealDB
+Future<void> connect({
+  required String url,
+  required String namespace,
+  required String database,
+  required SignInStrategy signInStrategy,
+}) async {
+  surrealDB = Surreal(url: url);
+  surrealDB.connect();
+  await surrealDB.wait();
+  await surrealDB.use(ns: namespace, db: database);
+  await surrealDB.signIn(signInStrategy);
+}
