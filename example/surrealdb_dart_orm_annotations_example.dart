da0a6d7f77747ace4257e87fedb0baa0ca43318c
tejHackerDev <tejasimha222@gmail.com>
2023-04-06 21:58:18 +0530

    Initial Commit
    Signed-off-by: tejHackerDev <tejasimha222@gmail.com>

---
+++ example/surrealdb_dart_orm_annotations_example.dart
@@ -0,0 +1,6 @@
+import 'package:surrealdb_dart_orm_annotations/surrealdb_dart_orm_annotations.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/surrealdb_dart_orm_annotations.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library surrealdb_dart_orm_annotations;
+
+export 'src/surrealdb_dart_orm_annotations_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
